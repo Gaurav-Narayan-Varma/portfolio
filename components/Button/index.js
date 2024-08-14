01f241f41dfe7cdee84d81ed733124a80c02a1eb
Gaurav Varma <gaurav.varma@hey.com>
2024-08-13 23:02:59 -0700

    fix: button hover bg matches dark mode

--- components/Button/index.js
+++ components/Button/index.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useTheme } from "next-themes";
 import data from "../../data/portfolio.json";
 
 const Button = ({ children, type, onClick, classes }) => {
   const { theme } = useTheme();
+  const [buttonClass, setButtonClass] = useState(
+    "hover:bg-slate-600 text-white"
+  );
   if (type === "primary") {
     return (
       <button
@@ -19,15 +22,19 @@ const Button = ({ children, type, onClick, classes }) => {
       </button>
     );
   }
+
+  useEffect(() => {
+    if (theme === "dark") {
+      setButtonClass("hover:bg-slate-600 text-white");
+    } else {
+      setButtonClass("hover:bg-slate-100");
+    }
+  }, [theme]);
   return (
     <button
       onClick={onClick}
       type="button"
-      className={`text-sm tablet:text-base p-1 laptop:p-2 m-1 laptop:m-2 rounded-lg flex items-center transition-all ease-out duration-300 ${
-        theme === "dark"
-          ? "hover:bg-slate-600 text-white"
-          : "hover:bg-slate-100"
-      } hover:scale-105 active:scale-100  tablet:first:ml-0  ${
+      className={`text-sm tablet:text-base p-1 laptop:p-2 m-1 laptop:m-2 rounded-lg flex items-center transition-all ease-out duration-300 ${buttonClass} hover:scale-105 active:scale-100  tablet:first:ml-0  ${
         data.showCursor && "cursor-none"
       } ${classes} link`}
     >
